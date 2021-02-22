94201cf85483dc027799b3c763b6b2d2b2884ef3
Frank Schüler <SchuelerFrank@gmx.de>
2021-02-22 12:36:11 +0100

    include browser.js at npm package bugfix at browser.js

--- browser.js
+++ browser.js
@@ -1,6 +1,8 @@
 import pack from "./src"
 import GLOBAL from "@modules/@default-js/defaultjs-common-utils/src/Global";
 
+
+GLOBAL.defaultjs = GLOBAL.defaultjs || {};
 GLOBAL.defaultjs.el = GLOBAL.defaultjs.el || (() => {
 	pack.VERSION = "${version}";
 	return pack;
